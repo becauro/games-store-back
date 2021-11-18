47ed10338e20074ba362816d78e16fb120489d69
Michel Pereira <michelbecauro@gmail.com>
2021-11-18 02:58:10 -0300

    chore: Adiciona middleware e error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const products = require('./controllers/products');
 const sales = require('./controllers/sales');
+const errorMiddleware = require('./utils/errorMiddleware');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use('/products', products);
 app.use('/sales', sales);
 
 app.all('*', (req, res) => res.status(404).send('Router not found'));
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log('Online');
