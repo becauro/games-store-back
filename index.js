f120bd5aac8a42cef9c9d1692cf0eb5fe6e0756e
Michel Pereira <michelbecauro@gmail.com>
2021-11-18 02:40:25 -0300

    chore: Atualiza variavel de ambiente

--- index.js
+++ index.js
@@ -2,22 +2,15 @@ const express = require('express');
 const products = require('./controllers/products');
 const sales = require('./controllers/sales');
 
-const PORT = '3000';
-
 const app = express();
 
 app.use(express.json());
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
 
 app.use('/products', products);
 app.use('/sales', sales);
 
 app.all('*', (req, res) => res.status(404).send('Router not found'));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log('Online');
 });
--- models/connection.js
+++ models/connection.js
@@ -6,7 +6,7 @@ const OPTIONS = {
    useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = process.env.DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = process.env.DB_URL;
  
 let db = null;
 
