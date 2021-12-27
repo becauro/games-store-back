29ef7ef69f4ba01e75c9d87920dfac4463452c4a
Michel Pereira <michelbecauro@gmail.com>
2021-12-27 02:50:02 -0300

    fix: Set CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const products = require('./controllers/products');
 const sales = require('./controllers/sales');
 const errorMiddleware = require('./utils/errorMiddleware');
@@ -7,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use('*', cors());
 
 app.use('/products', products);
 app.use('/sales', sales);
