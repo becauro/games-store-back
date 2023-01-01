95ad2a8588359806410daa2cf6dcd2f0e81654d7
Michel Pereira <michelbecauro@gmail.com>
2023-01-01 07:43:05 -0300

    FEAT: It makes db URI dynamic in models/connection

--- models/connection.js
+++ models/connection.js
@@ -7,7 +7,7 @@ const OPTIONS = {
    useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = process.env.DB_URL;
+const MONGO_DB_URL = process.env.DB_URL || "mongodb://localhost:27017/StoreManager";
  
 let db = null;
 
