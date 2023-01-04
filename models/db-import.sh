a823a2fd0c4705fb3dac0c2c2ad61f333972a4be
Michel Pereira <michelbecauro@gmail.com>
2023-01-04 13:30:05 -0300

    FIX: var value in models/db-import

--- models/db-import.sh
+++ models/db-import.sh
@@ -2,8 +2,8 @@
 
 cd /DataForImport
 
-mongo --eval "var mongo_name = '$DB_NAME'"  dataTestForDb.js
-
+mongo --eval "var mongo_name = '$DB_NAME'"  dataTestForDb.js > /dev/null 
+test $? -eq 0 || mongo --eval "var mongo_name = 'GamesStore'"  dataTestForDb.js > /dev/null 
 echo "EXECUTADO SCRIPT db-import.sh"
 
 
