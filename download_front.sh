bde1f840497ed0a9c446fb67c3354d55099b5db6
Michel Pereira <michelbecauro@gmail.com>
2023-01-26 09:38:05 -0300

    FEAT: Finish Unziping (step 3) in download_front.sh

--- download_front.sh
+++ download_front.sh
@@ -9,12 +9,12 @@ printf "1 - Verifying if unzip is installed ...\n\n";
 
   if test $? -ne 0 ; then    # bash POSIX >>  if [[ $(test $? -ne 0) ]] 
 
-    printf "\t FAIL - It seems unzip is not installed. Let's install it now ...\n\n" ;
+    printf "  FAIL - It seems unzip is not installed. Let's install it now ...\n\n" ;
     printf "  1.1 - Installing unzip ...\n\n"; 
 
       apt-get install unzip -y > /dev/null 2>&1 ;
         if ! test $? -eq 0 ; then 
-          printf "\t FAIL - Installation FAILED. \n\t Please, run this script with elevated privileges OR trying install unzip manually then go back here :) \n\n" ; 
+          printf "  FAIL - Installation FAILED. \n\t Please, run this script with elevated privileges OR trying install unzip manually then go back here :) \n\n" ; 
           printf "See ya! \n\n";
           exit 1;
         else
@@ -32,7 +32,7 @@ printf "2 - Downloading repository ...\n\n" ;
 
   if test $? -ne 0 ; then 
 
-    printf "\t FAIL - download FAILED. \n\t Please, check parent folder permissions then go back here OR trying download repo manually :) \n\n" ; 
+    printf "  FAIL - download FAILED. \n\t Please, check parent folder permissions then go back here OR trying download repo manually :) \n\n" ; 
     printf "Exiting...! \n\n";
     exit 1;
 
@@ -45,4 +45,18 @@ printf "2 - Downloading repository ...\n\n" ;
 
 printf "3 - Unziping file ... \n\n" ;
 
-  unzip front-repo -d .. # && ../games-store-front-main ../games-store-front 
+  unzip front-repo -d .. > /dev/null 2>&1 ; # && ../games-store-front-main ../games-store-front 
+
+  if test $? -ne 0 ; then 
+
+    printf "  FAIL - unziping FAILED. \n\t Please, check the current/parent folder permissions, run this script with elevated privileges \
+    OR trying unzip the file manually :) \n\n" ; 
+
+    printf "Exiting...! \n\n";
+    exit 1;
+
+  else 
+    
+    printf "  OK - file unziped! \n\n" ;
+
+  fi
