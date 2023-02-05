64f2a376891eda814ba6bd1f4c2893f4d7462cf0
Michel Pereira <michelbecauro@gmail.com>
2023-02-05 14:05:09 -0300

    CHORE: Implements path.join()

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
 const products = require('./controllers/products');
 const sales = require('./controllers/sales');
 const errorMiddleware = require('./utils/errorMiddleware');
@@ -8,15 +9,16 @@ const errorMiddleware = require('./utils/errorMiddleware');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 app.use(express.json());
 
 app.use(cors());
 
+app.use(express.static(path.join(`${__dirname}`, '/images')));
+
 app.use('/products', products);
 app.use('/sales', sales);
 
-app.all('*', (req, res) => res.status(404).send('Router not found'));
+app.all('*', (_req, res) => res.status(404).send('Router not found'));
 
 app.use(errorMiddleware);
 
